58380e514e567eb1dcda22126bebd9d9223da398
Pratik raj <pratikraj9117@gmail.com>
2025-11-21 22:50:51 +0530

    deploy

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ connectDB();
 
 app.use(cors({
   origin: "http://localhost:5173",
+  "https://craft-vistaar.vercel.app/",
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
